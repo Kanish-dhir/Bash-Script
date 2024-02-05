800432f3b71a2d5ad49e0a8af85bbd01232c081b
Kanish08 <105987397+Kanish-dhir@users.noreply.github.com>
2024-02-06 01:31:44 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
  #!/bin/bash
  #script- print no divisible by 3 & 5 not 15
+ for i in {1..100}; do
    if (['expr $i % 2' == 0] || ['expr $i % 5 == 0] && ['expr &i % 15 != 0]);
    then
          echo $i
